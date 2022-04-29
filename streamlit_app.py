4b01052d8f4ec24fe3ace393619a73aae8135b07
EmmanuelEA <eescobar@ingenieros.com>
2022-04-28 23:55:01 -0500

    Cambio de psycopg2 to sqlachemy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,17 @@
 import streamlit as st
 import pandas as pd
-#from sqlalchemy import create_engine
-import psycopg2
-from psycopg2 import DatabaseError
+from sqlalchemy import create_engine
+#import psycopg2
+#from psycopg2 import DatabaseError
 
-try:
-    connection = psycopg2.connect(
-        host='platzicohort10.cig2rbjhhqmz.us-east-1.rds.amazonaws.com',
-        user='usuario_consulta',
-        password='platzicohort10',
-        database='Brazilian_e_commerce'
-    )
+engine = create_engine('postgresql://usuario_consulta:platzicohort10@platzicohort10.cig2rbjhhqmz.us-east-1.rds.amazonaws.com/Brazilian_e_commerce')
 
-    print("Conexión exitosa.")
-    cursor = connection.cursor()
-    #cursor.execute("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
-    cursor.execute("SELECT product_id,price FROM olist_order_items_dataset WHERE price > 3000")
-    row = cursor.fetchall()
-    #print(format(row))
-    df = pd.DataFrame(row)
-    df.columns = ['product_id','price']
-    df = df.astype(str) 
-    #print(df.head())
-    #print(df.dtypes)
-except DatabaseError as ex:
-    print("Error durante la conexión: {}".format(ex))
-finally:
-    connection.close()  # Se cerró la conexión a la BD.
-    print("La conexión ha finalizado.")
-
+with engine.connect() as con:
+  rs = con.execute("SELECT product_id,price FROM olist_order_items_dataset WHERE price > 3000") # query que vamos a realizar
+  df = pd.DataFrame(rs.fetchall()) # lectura de las filas, hay mas opciones
+  df.columns = rs.keys()
+#  df.columns = ['product_id','price']
+ 
 
 st.dataframe(data=df)
 st.bar_chart(data=df)

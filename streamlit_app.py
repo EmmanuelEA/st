4440fd34257fb80343b419a195b5854c2366b799
EmmanuelEA <eescobar@ingenieros.com>
2022-04-30 22:46:49 -0500

    con dark palette

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,17 @@ with engine.connect() as con:
   rs = con.execute("SELECT product_id,price FROM olist_order_items_dataset WHERE price > 6000") # query que vamos a realizar
   df = pd.DataFrame(rs.fetchall()) # lectura de las filas, hay mas opciones
   df.columns = rs.keys()
-  #df = df.astype(str)
 
+
+df = df.sort_values(by=['price'], ascending=True)
 # Plot the fur data using Seaborn's countplot
 fig, ax = plt.subplots(figsize=(10, 5))
-ax = sns.lineplot(df)
-
+sns.set_style("dark")
+ax = sns.histplot(data=df, x='price', y='product_id', hue='product_id')
 
 
 st.title('Datos de AWS, tabla olist_order_items_dataset')
-st.text('consulta product_id con los precios mayores a 6000')
+st.text('consulta product_id con los precios mayores a 5000')
 st.dataframe(data=df)
 st.pyplot(fig)
-#st.bar_chart(data=df)
+st.bar_chart(data=df)
